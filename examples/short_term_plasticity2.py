a97f94263b421872ad7eb9f3f628ce5321694179
Romain Brette <romain.brette@ens.fr>
2009-01-25 10:56:15 +0000

    STP examples updated

--- examples/short_term_plasticity2.py
+++ examples/short_term_plasticity2.py
@@ -3,6 +3,7 @@ Network (CUBA) with depressing synapses
 Excitatory synapses are depressing, inhibitory ones are not
 '''
 from brian import *
+from time import time
 
 U_SE=.2
 tau_rec=500*ms
@@ -11,31 +12,24 @@ eqs='''
 dv/dt = (ge+gi-(v+49*mV))/(20*ms) : volt
 dge/dt = -ge/(5*ms) : volt
 dgi/dt = -gi/(10*ms) : volt
-dR/dt=(1-R)/tau_rec : 1 # synaptic resource (in [0,1])
 '''
 
-def reset_STP(P,spikes):
-    P.R_[spikes]-=U_SE*P.R_[spikes]
-    P.v_[spikes]=-60*mV
-
-P=NeuronGroup(4000,model=eqs,threshold=-50*mV,reset="R-=U_SE*R;v=-60*mV")
+P=NeuronGroup(4000,model=eqs,threshold=-50*mV,reset=-60*mV)
 P.v=-60*mV+rand(4000)*10*mV
-P.R=1
 Pe=P.subgroup(3200)
 Pi=P.subgroup(800)
-Ce=Connection(Pe,P,'ge',modulation='R')
-Ci=Connection(Pi,P,'gi')
-Ce.connect_random(Pe, P, 0.02,weight=1.62*mV)
-Ci.connect_random(Pi, P, 0.02,weight=-9*mV)
+Ce=Connection(Pe,P,'ge',weight=1.62*mV,sparseness=.02)
+Ci=Connection(Pi,P,'gi',weight=-9*mV,sparseness=.02)
+stp=STP(Ce,taud=tau_rec,tauf=.1*ms,U=U_SE)
 M=SpikeMonitor(P)
-trace=StateMonitor(P,'R',record=0)
 rate=PopulationRateMonitor(P)
+t1=time()
 run(1*second)
+t2=time()
+print "Simulation time:",t2-t1,"s"
 print M.nspikes,"spikes"
-subplot(311)
+subplot(211)
 raster_plot(M)
-subplot(312)
-plot(trace.times/ms,trace[0])
-subplot(313)
+subplot(212)
 plot(rate.times/ms,rate.smooth_rate(5*ms))
 show()
