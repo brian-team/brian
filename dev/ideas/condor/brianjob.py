919fd7bf7f47224b1c47ba033cd50e1b4bdc898d
Romain Brette <romain.brette@ens.fr>
2009-02-11 08:48:42 +0000

    Condor test

---
+++ dev/ideas/condor/brianjob.py
@@ -0,0 +1,12 @@
+'''
+This a job to be executed by Condor
+'''
+from brian import *
+import sys
+
+#arg=sys.argv[1]
+
+print "Hi everyone!"
+print "Here is a random number",randint(100)
+#print "I found",arg
+#print "I am doubling it:",2*int(arg)
