d971c11e29195e9fe217973346f8508be84e490c
Romain Brette <romain.brette@ens.fr>
2009-12-21 11:57:26 +0000

    Added tests for neurongroup (poissongroup)

---
+++ brian/tests/testinterface/test_neurongroup.py
@@ -0,0 +1,19 @@
+from brian import *
+from brian.utils.approximatecomparisons import *
+from nose.tools import *
+
+def test():
+    reinit_default_clock()
+    
+    # PoissonGroup
+    G=PoissonGroup(10,rates=10*Hz)
+    G1=G[:5]
+    G2=G[5:]
+    G1.rate=5*Hz
+    G2.rate=20*Hz
+    assert(is_approx_equal(G.rate[0],float(5*Hz)))
+    assert(is_approx_equal(G1.rate[0],float(5*Hz)))
+    assert(is_approx_equal(G2.rate[0],float(20*Hz)))
+
+if __name__=='__main__':
+    test()
