64d99116ea744581245e8675ce5e1cda2695e623
Dan Goodman <dan.goodman@ens.fr>
2010-02-13 19:31:35 +0000

    Cleaned up dev folder a bit, deleted old stuff

--- dev/debugging/refractory_fix.py
+++
@@ -1,9 +0,0 @@
-from brian import *
-
-G = NeuronGroup(1, 'v:1', threshold=-1, refractory=1*ms)
-
-M = SpikeMonitor(G)
-
-run(10*ms)
-
-print M.spikes
--- dev/ideas/patchtest.py
+++
@@ -1,5 +0,0 @@
-# Here is a try for producing/applying patches
-from brian import *
-
-print "bonjour"
-print "almost done"
