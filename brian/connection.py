f30cadbe13da982d41aa076525cfd06f8e696b7c
Romain Brette <romain.brette@ens.fr>
2010-04-01 08:20:13 +0000

    connect_random: both weight and sparseness can be functions.

--- brian/connection.py
+++ brian/connection.py
@@ -1543,7 +1543,10 @@ class Connection(magic.InstanceTracker, ObjectContainer):
         if callable(weight):
             # Check units
             try:
-                weight(0,0)+Q._S0[self.nstate]
+                if weight.func_code.co_argcount==2:
+                    weight(0,0)+Q._S0[self.nstate]
+                else:
+                    weight()+Q._S0[self.nstate]
             except DimensionMismatchError,inst:
                 raise DimensionMismatchError("Incorrects unit for the synaptic weights.",*inst._dims)
             self.connect(P,Q,random_matrix_function(len(P),len(Q),p,value=weight))
@@ -2107,9 +2110,46 @@ def random_matrix(n,m,p,value=1.):
     If value is a function, then that function is called for each
     non zero element as value() or value(i,j).
     '''
+    # TODO:
+    # Simplify (by using valuef)
     W=sparse.lil_matrix((n,m))
     if callable(value) and callable(p):
-        raise AttributeError,"Arguments value and p cannot both be functions (yet)"
+        if value.func_code.co_argcount==0:
+            valuef=lambda i,j:[value() for _ in j] # value function
+        elif value.func_code.co_argcount==2:
+            try:
+                failed=(array(value(0,arange(m))).size!=m)
+            except:
+                failed= True
+            if failed: # vector-based not possible
+                log_debug('connections','Cannot build the connection matrix by rows')
+                valuef=lambda i,j:[value(i,k) for k in j]
+            else:
+                valuef=value
+        else:
+            raise AttributeError,"Bad number of arguments in value function (should be 0 or 2)"
+        
+        if p.func_code.co_argcount==2:
+            # Check if p(i,j) is vectorisable
+            try:
+                failed=(array(p(0,arange(m))).size!=m)
+            except:
+                failed= True
+            if failed: # vector-based not possible
+                log_debug('connections','Cannot build the connection matrix by rows')
+                for i in xrange(n):
+                    W.rows[i]=[j for j in range(m) if rand()<p(i,j)]
+                    W.data[i]=list(valuef(i,array(W.rows[i])))
+            else: # vector-based possible
+                for i in xrange(n):
+                    W.rows[i]=list((rand(m)<p(i,arange(m))).nonzero()[0])
+                    W.data[i]=list(valuef(i,array(W.rows[i])))
+        elif p.func_code.co_argcount==0:
+            for i in xrange(n):
+                W.rows[i]=[j for j in range(m) if rand()<p()]
+                W.data[i]=list(valuef(i,array(W.rows[i])))
+        else:
+            raise AttributeError,"Bad number of arguments in p function (should be 2)"
     elif callable(value):
         if value.func_code.co_argcount==0: # TODO: should work with partial objects
             for i in xrange(n):
---
+++ dev/debugging/randommatrix.py
@@ -0,0 +1,17 @@
+from brian import *
+
+P=NeuronGroup(100,model='dv/dt=0*Hz:1')
+Q=NeuronGroup(200,model='dv/dt=0*Hz:1')
+C=Connection(P,Q,'v')
+C.connect_random(P,Q,sparseness=lambda i,j:exp(-.05*abs(i-j)),weight=lambda i,j:abs(cos(.1*(i-j))))
+C2=Connection(P,Q,'v')
+C2.connect_random(P,Q,sparseness=lambda i,j:exp(-.05*abs(i-j)),weight=lambda: rand())
+
+print C.W.nnz,C2.W.nnz
+
+figure()
+subplot(211)
+imshow(C.W.todense(), interpolation='nearest', origin='lower')
+subplot(212)
+imshow(C2.W.todense(), interpolation='nearest', origin='lower')
+show()
