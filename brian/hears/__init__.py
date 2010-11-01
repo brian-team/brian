d91995f6100d1ca838a1e45770197a562abcdf48
Dan Goodman <dan.goodman@ens.fr>
2010-11-01 23:03:41 +0000

    Added brian.hears.new subpackage for the new version with multichannel sounds and buffering
    Should be mostly backwards compatible, but for the moment I'm leaving it as optional, set the global preference usenewbrianhears=True to use it.

--- brian/hears/__init__.py
+++ brian/hears/__init__.py
@@ -1,6 +1,12 @@
-from .sounds import *
-from .erb import *
-from .filtering import *
-from .hrtf import *
+import brian as _brian
+if _brian.get_global_preference('usenewbrianhears'):
+    import warnings as _warnings
+    _warnings.warn('Using new version of Brian hears')
+    from .new import *
+else:
+    from .sounds import *
+    from .erb import *
+    from .filtering import *
+    from .hrtf import *
 
-dB = 1.
+    dB = 1.
---
+++ brian/hears/new/__init__.py
@@ -0,0 +1,6 @@
+from .sounds import *
+from .erb import *
+from .filtering import *
+from .hrtf import *
+
+dB = 1.
