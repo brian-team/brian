1058408bea362e7ac50da14d72bdab46c8a1fcf6
Dan Goodman <dan.goodman@ens.fr>
2010-12-01 17:11:21 +0000

    New Brian hears: fixed erbspace

--- brian/hears/newversion/erb.py
+++ brian/hears/newversion/erb.py
@@ -16,20 +16,18 @@ def erbspace(low, high, N, earQ=9.26449, minBW=24.7, order=1):
         Number of channels
     ``earQ=9.26449``, ``minBW=24.7``, ``order=1``
         Default Glasberg and Moore parameters.
-        
-    TODO: This function doesn't return high as one of the parameters, introduce
-    a new keyword endpoint to say whether or not to include the high end?
-    What should a -space function do? e.g. linspace/logspace?
     '''
     low = float(low)
     high = float(high)
-    cf = -(earQ * minBW) + exp((arange(1, N + 1)) * (-log(high + earQ * minBW) + \
-            log(low + earQ * minBW)) / N) * (high + earQ * minBW)
+    cf = -(earQ * minBW) + exp((arange(N)) * (-log(high + earQ * minBW) + \
+            log(low + earQ * minBW)) / (N-1)) * (high + earQ * minBW)
     cf = cf[::-1]
     return cf
 
 # Testing
 if __name__ == '__main__':
     cf = erbspace(20 * Hz, 20 * kHz, 3000)
+    print amin(cf), amax(cf)
+    print diff(cf)[-5:]
     plot(cf)
     show()
