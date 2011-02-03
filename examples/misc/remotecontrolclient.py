f3a159f6e1c65c8055a53becd5c72937c94b8a21
Dan Goodman <dan.goodman@ens.fr>
2011-02-03 14:54:41 +0000

    More doc updates and new examples

---
+++ examples/misc/remotecontrolclient.py
@@ -0,0 +1,26 @@
+'''
+Example of using :class:`RemoteControlServer` and :class:`RemoteControlClient`
+to control a simulation as it runs in Brian.
+
+Run the script remotecontrolserver.py before running this.
+'''
+from brian import *
+import time
+
+client = RemoteControlClient()
+
+time.sleep(1)
+
+subplot(121)
+plot(*client.evaluate('(M.times, M.values)'))
+
+client.execute('G.I = 1.1')
+
+time.sleep(1)
+
+subplot(122)
+plot(*client.evaluate('(M.times, M.values)'))
+
+client.stop()
+
+show()
---
+++ examples/misc/remotecontrolserver.py
@@ -0,0 +1,20 @@
+'''
+Example of using :class:`RemoteControlServer` and :class:`RemoteControlClient`
+to control a simulation as it runs in Brian.
+
+After running this script, run remotecontrolclient.py or paste the code from
+that script into an IPython shell for interactive control.
+'''
+from brian import *
+
+eqs = '''
+dV/dt = (I-V)/(10*ms)+0.1*xi*(2/(10*ms))**.5 : 1
+I : 1
+'''
+
+G = NeuronGroup(3, eqs, reset=0, threshold=1)
+M = RecentStateMonitor(G, 'V', duration=50*ms)
+
+server = RemoteControlServer()
+
+run(1e10*second)
