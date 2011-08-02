e90ea4c3961dae0a1cd09072aea0b716d27c5d14
Romain Brette <romain.brette@ens.fr>
2011-08-02 07:51:06 +0000

    Spike queue: works more or less

--- dev/ideas/spikequeue/spikequeue.py
+++ dev/ideas/spikequeue/spikequeue.py
@@ -49,21 +49,31 @@ class SpikeQueue(object):
         # That's not a very efficient way to do it
         # (it's O(n*log(n)))
         # (not tested!)
-        n_synapses_per_timestep=bincount(delays)
-        ofs=hstack([arange(x) for x in n_synapses_per_timestep]) #very slow
-        return ofs[argsort(delay)]
-        # argsort is wrong (it's the converse) - multiply and add arange maybe?
+        I = argsort(delay)
+        xs = delay[I]
+        J = xs[1:]!=xs[:-1]
+        #K = xs[1:]==xs[:-1]
+        A = hstack((0, cumsum(J)))
+        #B = hstack((0, cumsum(K)))
+        B = hstack((0, cumsum(-J)))
+        BJ = hstack((0, B[J]))
+        ei = B-BJ[A]
+        ofs = zeros_like(delay)
+        ofs[I] = ei
+        return ofs
         
     def insert(self,delay,offset,target):
         # Vectorized insertion of spike events
         # delay = delay in timestep
         # offset = offset within timestep
         # target = target synaptic index
+        timesteps=(self.currenttime+delay) % len(self.n)
         self.X_flat[(self.currenttime*self.X.shape[1]+offset+\
-                     self.n[(self.currenttime+delay) % len(self.n)])\
+                     self.n[timesteps])\
                      % len(self.X)]=target
         # Update the size of the stacks
-        self.n[(self.currenttime+delay) % len(self.n)]+=offset+1 # that's a trick
+        self.n[timesteps]+=offset+1 # that's a trick
+        # There should a re-sizing operation, if overflow
 
 '''
 The connection has arrays of synaptic variables (same as state matrix of
@@ -88,11 +98,16 @@ Total memory:
 
 if __name__=='__main__':
     queue=SpikeQueue(5,30)
-    delays=array([1,1,3,9,2,2,2,8,7])
-    print argsort(delays)
+    Nsynapses=4000*80 # in the CUBA example
+    nspikes=160
+    delays=randint(160,size=nspikes) # average number of spikes per dt in CUBA
+    targets=randint(Nsynapses,size=nspikes)
+    #print queue.offsets(delays)
     t1=time()
-    for _ in range(10000):
+    for _ in range(10000): # 10000 timesteps per second
         d=queue.offsets(delays)
+        queue.insert(delays,d,targets)
+        queue.next()
+        events=queue.peek()
     t2=time()
-    print d
     print t2-t1
