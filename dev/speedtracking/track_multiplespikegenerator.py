894e0f84f3d0b3f33f1454313ba8d6ff74ccb4fe
Marcel Stimberg <marcel.stimberg@ens.fr>
2011-12-02 15:35:09 +0000

    track speed for different kind of arguments, add speed tracking for MultipleSpikeGenerator

---
+++ dev/speedtracking/track_multiplespikegenerator.py
@@ -0,0 +1,58 @@
+speedtrack_desc = 'MultiSpikeGeneratorGroup with a fixed array of spiketimes'
+
+from brian import *
+from tracking import *
+import gc
+
+# All neurons fire simultaneously with the given rate
+# The spiketimes are either given as a list of (index, time) pairs or as an 
+# array with first column index and second column time  
+
+# Firing rates
+rates = [1, 500, 1000, 2000, 5000] #Hz
+
+def run_barrage():
+    results = []    
+    for r in rates:                    
+        print 'Rate: %dHz ' % (r)
+        t = TrackSpeed(r)
+        results.append((str(r), track(t), r))
+        gc.collect()
+    return (speedtrack_desc, results)
+
+def plot_results(name, results):
+    _, t, arg = zip(*results)
+    plot(arg, t, 'o-')
+    xlabel('rate (Hz)')
+    ylabel('time (s)')
+    title(name)
+
+
+class TrackSpeed(object):
+             
+    def __init__(self, rate, duration=1): 
+        
+        N = 100
+        dt = defaultclock.dt
+        defaultclock.reinit()
+        self.duration = duration
+        
+        #The spike times are shifted by half a dt to center the spikes in the bins
+        times = arange(0, duration, 1./rate)             
+        spiketimes = [[t*second + dt/2 for t in times] for n in xrange(N)]          
+        
+        G = MultipleSpikeGeneratorGroup(spiketimes)
+
+        self.net = Network(G)
+        self.net.prepare()
+
+    def run(self):
+        self.net.run(self.duration * second)
+
+if __name__ == '__main__':
+    name, results = run_barrage()
+    print name
+    for desc, t, args in results:
+        print desc, args, ':', t
+    plot_results(name, results)
+    show()
--- dev/speedtracking/track_spikegenerator.py
+++ dev/speedtracking/track_spikegenerator.py
@@ -4,39 +4,34 @@ from brian import *
 from tracking import *
 import gc
 
-#Two conditions are tested:
-#    * all neurons spiking with a certain density
-#    * only one neuron (drawn randomly for every spike) spikes
+# All neurons fire simultaneously with the given rate
+# The spiketimes are either given as a list of (index, time) pairs or as an 
+# array with first column index and second column time  
 
 # Firing rates
-rates = [1, 1000, 2500, 5000, 10000] #Hz
+rates = [1, 500, 1000, 2000, 5000] #Hz
 
-# dt is also varied but should not make a difference
-dts = [0.1, 0.01] #ms
-
-# All neurons fire vs. one neuron fires
-stim_types = ['all', 'random']
+# Spike times are given as pairs, as pairs but using gather=True, or as an array 
+stim_types = ['pairs', 'pairs_gather', 'array']
 
 def run_barrage():
     results = []
     
     for stim_type in stim_types:
         for r in rates:
-            for dt in dts:        
-                t = TrackSpeed(r, stim_type)
-                results.append((str(r), track(t), (r, stim_type, dt)))
-                gc.collect()
+            print stim_type, str(r)
+            t = TrackSpeed(r, stim_type)
+            results.append(('%s (%dHz)' % (stim_type, r), track(t),
+                            (r, stim_type)))
+            gc.collect()
     return (speedtrack_desc, results)
 
 def plot_results(name, results):
     for stim_type in stim_types:
-        for dt in dts:
-            allt = [t for _, t, args in results 
-                    if args[1] == stim_type and args[2] == dt]
-            alln = [args[0] for _, t, args in results 
-                    if args[1] == stim_type and args[2] == dt]
-            plot(alln, allt, 'o-', label=('%s (dt=%.2fms)' % (stim_type, dt)))
-    legend()
+        allt = [t for _, t, args in results  if args[1] == stim_type]
+        alln = [args[0] for _, t, args in results if args[1] == stim_type]
+        plot(alln, allt, 'o-', label=(stim_type))
+    legend(frameon=False)
     xlabel('rate (Hz)')
     ylabel('time (s)')
     title(name)
@@ -53,14 +48,20 @@ class TrackSpeed(object):
         
         #The spike times are shifted by half a dt to center the spikes in the bins
         times = arange(0, duration, 1./rate)             
-        if stim_type == 'all':            
+        gather = False
+        
+        if stim_type == 'pairs':            
             spiketimes = [(idx, t*second +dt/2) for idx in xrange(N) for t in times]            
-        elif stim_type == 'random':
-            spiketimes = [(randint(N), t*second + dt/2) for t in times]
-            pass
+        elif stim_type == 'pairs_gather':
+            spiketimes = [(idx, t*second +dt/2) for idx in xrange(N) for t in times]
+            gather = True
+        elif stim_type == 'array':
+            spiketimes = vstack([array([n, t]) for t in times for n in xrange(N)])          
         else:
-            raise ValueError('Unknown stim_type (should be "all" or "random')
-        G = SpikeGeneratorGroup(N, spiketimes)
+            raise ValueError('Unknown stim_type (should be "pairs",' + \
+                             '"pairs_gather", "array", or "multi")')            
+        
+        G = SpikeGeneratorGroup(N, spiketimes, gather=gather)
 
         self.net = Network(G)
         self.net.prepare()
@@ -72,6 +73,6 @@ if __name__ == '__main__':
     name, results = run_barrage()
     print name
     for desc, t, args in results:
-        print desc, args, ':', t
+        print desc, ':', t
     plot_results(name, results)
     show()
