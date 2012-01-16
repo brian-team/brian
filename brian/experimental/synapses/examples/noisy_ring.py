1760d70b77bf958dda5daaf8858db8eeddff9dc6
Romain Brette <romain.brette@ens.fr>
2012-01-16 15:56:49 +0000

    Synapses examples in experimental

---
+++ brian/experimental/synapses/examples/noisy_ring.py
@@ -0,0 +1,33 @@
+'''
+Integrate-and-fire neurons with noise
+'''
+from brian import *
+from brian.experimental.synapses import *
+
+tau = 10 * ms
+sigma = .5
+N = 100
+J = -1
+mu = 2
+
+eqs = """
+dv/dt=mu/tau+sigma/tau**.5*xi : 1
+"""
+
+group = NeuronGroup(N, model=eqs, threshold=1, reset=0)
+
+C = Synapses(group, model='w:1', pre='v+=w')
+C[:,:]='j==((i+1)%N)'
+C.w=J
+
+S = SpikeMonitor(group)
+trace = StateMonitor(group, 'v', record=True)
+
+run(500 * ms)
+i, t = S.spikes[-1]
+
+subplot(211)
+raster_plot(S)
+subplot(212)
+plot(trace.times / ms, trace[0])
+show()
---
+++ brian/experimental/synapses/examples/probabilistic_synapses.py
@@ -0,0 +1,31 @@
+"""
+Probabilistic synapses
+
+Seems to work.
+"""
+from brian import *
+from brian.experimental.synapses import *
+
+#log_level_debug()
+
+N=20
+tau=5*ms
+input=PoissonGroup(2,rates=20*Hz)
+neurons=NeuronGroup(N,model='dv/dt=-v/tau : 1')
+
+S=Synapses(input,neurons,model="""w : 1
+                                  p : 1 # transmission probability""",
+                         pre="v+=w*(rand()<p)")
+# Transmission probabilities
+S[:,:]=True
+S.w=0.5
+S.p[0,:]=linspace(0,1,N) # transmission probability between 0 and 1
+S.p[1,:]=linspace(0,1,N)[::-1] # reverse order for the second input
+
+M=StateMonitor(neurons,'v',record=True)
+
+run(500*ms)
+
+for i in range(N):
+    plot(M.times/ms,M[i]+i,'k')
+show()
