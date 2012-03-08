5f67871d916111844278c3717cbae18a19cbadf8
Marcel Stimberg <marcel.stimberg@ens.fr>
2012-03-08 15:46:26 +0000

    Treat examples as unit tests, using the nose framework. Publish x-unit and coverage report

--- dev/jenkins/linux_scripts/run_examples.sh
+++ dev/jenkins/linux_scripts/run_examples.sh
@@ -3,5 +3,6 @@ export PYTHONPATH=$(pwd)/build/lib:$PYTHONPATH
 cd dev/tests/
 rm examples_completed.txt || :
 
-# Do not fail if one of the examples fails, instead the run will be marked as "unstable" by the text-finder plugin
+# Do not fail if one of the examples fails, instead the run will be marked as
+# "unstable" by the violations plugin
 python -u allexamples_cumulative.py || :
--- dev/tests/allexamples_cumulative.py
+++ dev/tests/allexamples_cumulative.py
@@ -1,20 +1,29 @@
 '''
-Runs all of the examples in the examples/ folder.
-
-This version 
+Runs all of the examples in the examples/ folder, except those excluded in 
+examples_excluded.txt or those run previously (as recorded in
+examples_completed.txt).
 
 Note: Uses the 'Agg' backend, therefore no plots are displayed on the screen and
 it is possible to run this script on a headless server.
 '''
 
-import os, glob, sys, StringIO, time, gc, fnmatch
+import os, nose, warnings
+from nose.plugins import Plugin
+from nose.plugins.capture import Capture
+from nose.plugins.logcapture import LogCapture
+from nose.plugins.cover import Coverage
+from nose.plugins.xunit import Xunit
+
 # Use the 'Agg' backend, normally used for saving plots to files
 # this has the advantage of no plots showing up and can be run 
 # without an X server or similar. This has to be done before importing brian
 # because brian imports pylab
 import matplotlib as _mpl
 _mpl.use('Agg')
-import brian
+
+# Suppress all warnings
+warnings.simplefilter('ignore')
+import brian # It seems if we import brian here, nose can capture the logging
 
 exclude_list = open('examples_exclude.txt', 'U').read().split('\n')
 exclude_list = [f for f in exclude_list if not f.startswith('#') and f]
@@ -24,69 +33,55 @@ if os.path.exists('examples_completed.txt'):
 else:
     examples_completed = []
 
-os.chdir('../../examples')
+class SelectFilesPlugin(Plugin):
+    '''
+    This plugin makes nose descend into all directories but skips files that
+    are mentioned in examples_exclude.txt or examples_completed.txt
+    '''
+    # no command line arg needed to activate plugin
+    enabled = True
+    name = "select-files"
 
+    def configure(self, options, conf):
+        pass # always on
 
-class GlobDirectoryWalker:
-    # a forward iterator that traverses a directory tree
+    def wantDirectory(self, dirname):
+        # we want all directories
+        return True
+    
+    def wantFile(self, filename):
+        # we only need the filename, not the path
+        filename = os.path.split(filename)[1]
+        if filename in exclude_list or filename in examples_completed:
+            return False
+        elif filename.endswith('.py'):
+            return True
+        else:
+            return False
+        
+class RecordCompletionPlugin(Plugin):
+    ''' 
+    This plugin saves the completed examples to the file examples_completed.txt,
+    already completed examples will be skipped on subsequent runs.
+    
+    Note that this is ignored by the automatic testing on the Jenkins server, as
+    the script in dev/jenkins/linux_scripts/run_examples.sh first deletes
+    examples_completed.txt
+    '''
+    
+    enabled = True
+    name = 'record-completion'
 
-    def __init__(self, directory, pattern="*"):
-        self.stack = [directory]
-        self.pattern = pattern
-        self.files = []
-        self.index = 0
+    def configure(self, options, conf):
+        pass # always on
 
-    def __getitem__(self, index):
-        while 1:
-            try:
-                file = self.files[self.index]
-                self.index = self.index + 1
-            except IndexError:
-                # pop next directory from stack
-                self.directory = self.stack.pop()
-                self.files = os.listdir(os.path.abspath(self.directory))
-                self.index = 0
-            else:
-                # got a filename
-                fullname = os.path.join(os.path.abspath(self.directory), file)
-                if os.path.isdir(fullname) and not os.path.islink(fullname):
-                    self.stack.append(fullname)
-                if fnmatch.fnmatch(file, self.pattern):
-                    return fullname
+    def afterImport(self, filename, module):
+        filename = os.path.split(filename)[1]
+        open('examples_completed.txt', 'a').write(filename + '\n')
+        
+argv = [__file__, '-v', '--logging-clear-handlers', '--with-xunit',
+        '--with-coverage', '--cover-html', '--cover-package=brian',
+        '../../examples']
 
-examplefilenames = [name for name in GlobDirectoryWalker('', '*.py') if all(fname not in name for fname in exclude_list)]
-exceptions = []
-starttime = time.time()
-stdout = sys.stdout
-stderr, sys.stderr = sys.stderr, StringIO.StringIO()
-for fname in examplefilenames:
-    if fname not in examples_completed:
-        try:
-            code = open(fname, 'U').read()
-            ns = {}
-            print 'Running example', fname,
-            brian.reinit_default_clock()
-            sys.stdout = StringIO.StringIO()
-            exec code in ns
-            del ns
-            gc.collect()
-            sys.stdout = stdout
-            print '[ok]'
-            examples_completed.append(fname)
-            open('../dev/tests/examples_completed.txt', 'w').write('\n'.join(examples_completed))
-        except Exception, inst:
-            sys.stdout = stdout
-            print '[error]'
-            exceptions.append((fname, inst))
-sys.stderr = stderr
-endtime = time.time()
-print
-if exceptions:
-    print 'Exceptions encountered:'
-    for fname, inst in exceptions:
-        print 'In file', fname, '-', inst
-    print
-    print len(examplefilenames) - len(exceptions), 'of', len(examplefilenames), 'ran OK, time taken', endtime - starttime, 'seconds'
-    sys.exit(1)
-else:
-    print 'OK, time taken', endtime - starttime, 'seconds'
+nose.main(argv=argv, plugins=[SelectFilesPlugin(), RecordCompletionPlugin(),
+                              LogCapture(), Capture(), Coverage(), Xunit()])
