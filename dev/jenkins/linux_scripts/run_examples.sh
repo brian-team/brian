a463bbf6c175ca82ba8aed148d70290054962276
Marcel Stimberg <marcel.stimberg@ens.fr>
2012-04-12 11:41:16 +0000

    make sure nose is installed

--- dev/jenkins/linux_scripts/run_examples.sh
+++ dev/jenkins/linux_scripts/run_examples.sh
@@ -3,6 +3,10 @@ export PYTHONPATH=$(pwd)/build/lib:$PYTHONPATH
 cd dev/tests/
 rm examples_completed.txt || :
 
+# get the newest version of nose
+BASEDIR=$(dirname $PYTHON_EXE)
+$BASEDIR/pip install --upgrade -I nose || :
+
 # Do not fail if one of the examples fails, instead the run will be marked as
 # "unstable" by the violations plugin
 python -u allexamples_cumulative.py || :
