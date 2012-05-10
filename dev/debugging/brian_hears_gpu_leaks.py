321736c5b24542420524e84c71b474f506905bd5
Marcel Stimberg <marcel.stimberg@ens.fr>
2012-05-10 15:13:35 +0000

    was a duplicate of linearfilterbank_memleak.py

--- dev/debugging/brian_hears_gpu_leaks.py
+++
@@ -1,9 +0,0 @@
-from brian import *
-set_global_preferences(brianhears_usegpu=True)
-from brian.hears import *
-
-sound = silence(1 * second)
-gfb = Gammatone(sound, 100)
-for i in xrange(1000):
-    gfb.process()
-    print i
