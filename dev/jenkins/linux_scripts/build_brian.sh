51f1d6c1b27b5597ca30ff3120a57d0b93f76136
Marcel Stimberg <marcel.stimberg@ens.fr>
2012-05-22 11:42:48 +0000

    do not reinstall packages all the time, ignore more rows in coverage reports

--- dev/jenkins/linux_scripts/build_brian.sh
+++ dev/jenkins/linux_scripts/build_brian.sh
@@ -3,16 +3,16 @@ echo Using $(which python)
 # if no system-wide packages are used, update numpy etc. to newest version
 if [ $packages = newest ]; then
   echo "Using newest available package versions"
-  ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install --upgrade -I numpy 
-  ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install --upgrade -I scipy sympy
-  ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install --upgrade -I matplotlib
+  ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install --upgrade numpy 
+  ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install --upgrade scipy sympy
+  ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install --upgrade matplotlib
 elif [ $packages = oldest ]; then
   echo "Using oldest available package versions supported by Brian"
-  ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install -I numpy==1.3.0 
+  ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install numpy==1.3.0 
   ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install http://sourceforge.net/projects/scipy/files/scipy/0.7.0/scipy-0.7.0.tar.gz/download
-  ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install -I sympy
+  ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install sympy
   # Brian depencies state matplotlib>=0.90.1 but 0.98.1 is the oldest version still available
-  ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install -I http://sourceforge.net/projects/matplotlib/files/matplotlib/matplotlib-0.98.1/matplotlib-0.98.1.tar.gz/download
+  ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install http://sourceforge.net/projects/matplotlib/files/matplotlib/matplotlib-0.98.1/matplotlib-0.98.1.tar.gz/download
 fi
 
 # Print the version numbers for the dependencies
