c49490641bf663f8d844d22b38947ab0e74ea57e
Marcel Stimberg <marcel.stimberg@ens.fr>
2012-06-05 14:53:01 +0000

    delete left-over compiled code from previous runs

--- dev/jenkins/linux_scripts/test_brian.sh
+++ dev/jenkins/linux_scripts/test_brian.sh
@@ -4,6 +4,10 @@
 # Use the previously built brian version
 export PYTHONPATH=$(pwd)/build/lib:$PYTHONPATH
 
+# delete remaining compiled code from previous runs
+echo deleting '~/.python*_compiled' if it exists
+rm -r ~/.python*_compiled || :
+
 # Run unit tests and record coverage but do not fail the build if anything goes wrong here
 ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/coverage -e || :
 ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/coverage run ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/nosetests --with-xunit --logging-clear-handlers --verbose brian/tests || :
