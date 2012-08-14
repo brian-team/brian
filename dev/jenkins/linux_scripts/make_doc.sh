e5f08e63123fabd04e0a574e8f9e7a0f3d25bc7a
Marcel Stimberg <marcel.stimberg@ens.fr>
2012-08-14 13:15:24 +0000

    fix issue with spaces in file names

--- dev/jenkins/linux_scripts/make_doc.sh
+++ dev/jenkins/linux_scripts/make_doc.sh
@@ -1,5 +1,7 @@
+#!/bin/bash -x
+
 # Use the source directory
-export PYTHONPATH=$(pwd)
+export PYTHONPATH="$(pwd)"
 
 # Generates the HTML and PDF documentation for brian
 cd dev/tools/docs
@@ -13,13 +15,13 @@ python build_latex.py || exit 1
 cd ../../..
 
 # Copy the PDF to the doc directory
-cp $DOCROOT/docs_sphinx/_latexbuild/Brian*.pdf $DOCROOT/docs/
+cp "$DOCROOT"/docs_sphinx/_latexbuild/Brian*.pdf "$DOCROOT"/docs/
 
 # Delete old docs zip file if it exists
 rm docs.zip 2> /dev/null || :
 
 # copy the docs to the directory served by the local webserver
-cp -r $DOCROOT/docs/* ~/www-doc/$DOCNAME
+cp -r "$DOCROOT"/docs/* ~/www-doc/"$DOCNAME"
 
 # Create new zip file
-zip -r -q -9 docs.zip $DOCROOT/docs/
+zip -r -q -9 docs.zip "$DOCROOT"/docs/
