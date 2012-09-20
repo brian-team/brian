c75c7cbe35a05f4735b6eb5098dc9b943d2dcd3f
Marcel Stimberg <marcel.stimberg@ens.fr>
2012-09-20 15:41:02 +0000

    [brian2] tests: directly run from source directory

--- dev/jenkins/linux_scripts/test_brian2.sh
+++ dev/jenkins/linux_scripts/test_brian2.sh
@@ -1,8 +1,8 @@
 # get the newest version of nose and coverage, ignoring installed packages
 /home/jenkins/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install --upgrade -I nose coverage || :
 
-# Use the previously built brian version
-export PYTHONPATH=$(pwd)/build/lib:$PYTHONPATH
+# Directly use the source directory (no setup.py yet)
+export PYTHONPATH=$(pwd):$PYTHONPATH
 
 # delete remaining compiled code from previous runs
 echo deleting '~/.python*_compiled' if it exists
