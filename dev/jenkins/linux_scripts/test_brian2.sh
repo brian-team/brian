15ba39e0bda4f5a13c32840a213fa5799852923e
Marcel Stimberg <marcel.stimberg@ens.fr>
2012-09-20 15:36:37 +0000

    [brian2] set up automatic testing

---
+++ dev/jenkins/linux_scripts/test_brian2.sh
@@ -0,0 +1,14 @@
+# get the newest version of nose and coverage, ignoring installed packages
+/home/jenkins/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install --upgrade -I nose coverage || :
+
+# Use the previously built brian version
+export PYTHONPATH=$(pwd)/build/lib:$PYTHONPATH
+
+# delete remaining compiled code from previous runs
+echo deleting '~/.python*_compiled' if it exists
+rm -r ~/.python*_compiled || :
+
+# Run unit tests and record coverage but do not fail the build if anything goes wrong here
+~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/coverage erase --rcfile=.coveragerc_brian2 || :
+~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/coverage run --rcfile=.coveragerc_brian2 ~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/nosetests --with-xunit --logging-clear-handlers --verbose brian2/tests || :
+~/.jenkins/virtual_envs/$PythonVersion/$packages/bin/coverage xml --rcfile=.coveragerc_brian2 || :
