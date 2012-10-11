18b5a6c29938219f8981b18d943c9b7360f87e71
Marcel Stimberg <marcel.stimberg@ens.fr>
2012-10-11 15:57:22 +0000

    [brian2] be sure to have all the dependencies when running the tests.

--- dev/jenkins/linux_scripts/test_brian2.sh
+++ dev/jenkins/linux_scripts/test_brian2.sh
@@ -4,6 +4,45 @@
 # Make sure pyparsing is installed
 /home/jenkins/.jenkins/virtual_envs/$PythonVersion/$packages/bin/pip install pyparsing
 
+# This is copied from the build_brian.sh script. When brian2 gets a setup.py script, this script
+# should be used and the following lines removed.
+##### From build_brian.sh
+# if no system-wide packages are used, update numpy etc. to newest version
+if [ $packages = newest ]; then
+  echo "Using newest available package versions"
+  bin/pip install --upgrade numpy 
+  bin/pip install --upgrade scipy sympy
+  bin/pip install --upgrade matplotlib
+elif [ $packages = oldest ]; then
+  echo "Using oldest available package versions supported by Brian"
+  bin/pip install numpy==1.4.1
+  # scipy 0.7 has a bug that makes it impossible to use weave, download the
+  # package and apply the patch before installing
+  mkdir downloads || :
+  wget -O downloads/scipy-0.7.0.tar.gz http://sourceforge.net/projects/scipy/files/scipy/0.7.0/scipy-0.7.0.tar.gz/download
+  cd downloads
+  tar xvf scipy-0.7.0.tar.gz
+  # get and apply patch
+  cd scipy-0.7.0
+  wget http://projects.scipy.org/scipy/raw-attachment/ticket/739/weave-739.patch
+  patch -p1 < weave-739.patch
+  # build scipy
+  ../../bin/python setup.py install
+  cd ../..
+  
+  bin/pip install sympy
+  # Brian depencies state matplotlib>=0.90.1 but 0.98.5.3 seems to be the oldest one installable
+  bin/pip install http://sourceforge.net/projects/matplotlib/files/matplotlib/matplotlib-0.98.5/matplotlib-0.98.5.3.tar.gz/download
+fi
+
+# Print the version numbers for the dependencies
+bin/python -c "import numpy; print 'numpy version: ', numpy.__version__"
+bin/python -c "import scipy; print 'scipy version: ', scipy.__version__"
+bin/python -c "import sympy; print 'sympy version: ', sympy.__version__"
+bin/python -c "import matplotlib; print 'matplotlib version: ', matplotlib.__version__"
+
+##### End build_brian.sh
+
 # Directly use the source directory (no setup.py yet)
 export PYTHONPATH="$(pwd)":$PYTHONPATH
 
