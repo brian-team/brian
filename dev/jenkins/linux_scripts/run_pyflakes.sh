19832c530c812052468623bbf504548f6b1650de
Marcel Stimberg <marcel.stimberg@ens.fr>
2012-11-06 10:36:38 +0000

    Do not check __init__.py files with pyflakes as it does complain a lot about wildcard imports and unused identifiers

--- dev/jenkins/linux_scripts/run_pyflakes.sh
+++ dev/jenkins/linux_scripts/run_pyflakes.sh
@@ -4,4 +4,4 @@ if [[ "$BRIAN_PACKAGE" == "" ]]; then
     BRIAN_PACKAGE=brian
 fi
 
-find $BRIAN_PACKAGE -not -path "$BRIAN_PACKAGE/tests/*" -name '*.py' -print0 | xargs -0 pyflakes > pyflakes_warnings.log 2> pyflakes_errors.log || :
+find $BRIAN_PACKAGE -not -path "$BRIAN_PACKAGE/tests/*" -name '*.py' -not -name '__init__.py' -print0 | xargs -0 pyflakes > pyflakes_warnings.log 2> pyflakes_errors.log || :
