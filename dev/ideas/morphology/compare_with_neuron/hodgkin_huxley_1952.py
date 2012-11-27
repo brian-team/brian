8503f07ccde829262a401948af0a7632bd15d7a5
Romain Brette <romain.brette@ens.fr>
2012-11-27 17:24:50 +0000

    SpatialNeuron: more tests, seems ok so far.

--- dev/ideas/morphology/compare_with_neuron/hodgkin_huxley_1952.py
+++ dev/ideas/morphology/compare_with_neuron/hodgkin_huxley_1952.py
@@ -10,11 +10,11 @@ defaultclock.dt=0.01*ms
 
 morpho=Cylinder(length=10*cm, diameter=2*238*um, n=1000, type='axon')
 
--65*mV
+vshift=-65*mV
 
-El = 10.613* mV
-ENa = 115*mV
-EK = -12 * mV
+El = 10.613* mV +vshift
+ENa = 115*mV +vshift
+EK = -12 * mV +vshift
 gl = 0.3 * msiemens / cm ** 2
 gNa = 120 * msiemens / cm ** 2
 gK = 36 * msiemens / cm ** 2
@@ -26,16 +26,17 @@ I:amp/cm**2 # applied current
 dm/dt=alpham*(1-m)-betam*m : 1
 dn/dt=alphan*(1-n)-betan*n : 1
 dh/dt=alphah*(1-h)-betah*h : 1
-alpham=(0.1/mV)*(-v+25*mV)/(exp((-v+25*mV)/(10*mV))-1)/ms : Hz
-betam=4.*exp(-v/(18*mV))/ms : Hz
-alphah=0.07*exp(-v/(20*mV))/ms : Hz
-betah=1./(exp((-v+30*mV)/(10*mV))+1)/ms : Hz
-alphan=(0.01/mV)*(-v+10*mV)/(exp((-v+10*mV)/(10*mV))-1)/ms : Hz
-betan=0.125*exp(-v/(80*mV))/ms : Hz
+v0=v-vshift : volt
+alpham=(0.1/mV)*(-v0+25*mV)/(exp((-v0+25*mV)/(10*mV))-1)/ms : Hz
+betam=4.*exp(-v0/(18*mV))/ms : Hz
+alphah=0.07*exp(-v0/(20*mV))/ms : Hz
+betah=1./(exp((-v0+30*mV)/(10*mV))+1)/ms : Hz
+alphan=(0.01/mV)*(-v0+10*mV)/(exp((-v0+10*mV)/(10*mV))-1)/ms : Hz
+betan=0.125*exp(-v0/(80*mV))/ms : Hz
 '''
 
 neuron = SpatialNeuron(morphology=morpho, model=eqs, Cm=1 * uF / cm ** 2, Ri=35.4 * ohm * cm)
-neuron.v=0*mV
+neuron.v=0*mV+vshift
 neuron.h=1
 neuron.m=0
 neuron.n=.5
@@ -48,6 +49,14 @@ run(3*ms)
 neuron.I=0*amp/cm**2
 run(50*ms)
 
-for i in range(10):
-    plot(M.times/ms,M[i*10]/mV)
+# Load Neuron data
+file=r'D:\My Dropbox\LocalEclipseWorkspace\Neuron\hh.dat'
+x,y,z=read_neuron_dat(file)
+
+subplot(211)
+plot(x,y,'b')
+plot(M.times/ms,M[0]/mV,'r')
+subplot(212)
+plot(x,z,'b')
+plot(M.times/ms,M[500]/mV,'r')
 show()
