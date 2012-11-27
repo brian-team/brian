ff1854fd40cc359bf799ba21cb287a066a452a74
Romain Brette <romain.brette@ens.fr>
2012-11-27 08:50:42 +0000

    Morphology: multiple branches (not finished)

--- brian/experimental/morphology/spatialneuron.py
+++ brian/experimental/morphology/spatialneuron.py
@@ -23,7 +23,7 @@ from brian.clock import guess_clock
 from itertools import count
 from brian.neurongroup import NeuronGroup
 from scipy.linalg import solve_banded
-from numpy import zeros, ones, isscalar, diag_indices
+from numpy import zeros, ones, isscalar, diag_indices, pi
 from numpy.linalg import solve
 import numpy
 try:
@@ -214,272 +214,78 @@ class SpatialStateUpdater_old(StateUpdater):
         '''
         return len(self.eqs)
     
-class SpatialStateUpdater_new(StateUpdater): # Modified by Remy
+class SpatialStateUpdater_new(StateUpdater):
     """
-    State updater for compartmental models.
+    State updater for compartmental models, with several branches.
     """
-    def __init__(self, neuron, clock=None): # unchanged
+    def __init__(self, neuron, clock=None):
         self.eqs = neuron._eqs
         self.neuron = neuron
         self._isprepared = False
         self._state_updater=neuron._state_updater # to update the currents
-        
-    def prepare_branch(self, morphology, mid_diameter,ante=0):
+
+    def cut_branches(self,morphology):
         '''
-        1) fill neuron.branches and neuron.index with information about the morphology of the neuron
-        2) change some wrong values in Aplus and Aminus. Indeed these were correct only if the neuron is a linear cable.
-            Knowledge of the morphology gives correct values.
-        3) fill neuron.bc (boundary conditions)
+        Recursively cut the branches by setting zero axial resistances.
         '''
-        branch = morphology.branch()
-        i=branch._origin # First compartment of the current branch
-        j= i + len(branch) - 2
-        endpoint = j + 1 # Last compartment of the current branch
-        self.neuron.index[i:endpoint+1] = self.neuron.BPcount # Branch index
-        children_number = 0
-        
-        #connections between branches
-        for x in (morphology.children):#parent of segment n isn't always n-1 at branch points. We need to change Aplus and Aminus
-            gc = 2 * msiemens/cm**2 # WTF???
-            startpoint = x._origin
-            # Next line is odd: says that at branching point, the diameter is half that of both branches
-            mid_diameter[startpoint] = .5*(self.neuron.diameter[endpoint]+self.neuron.diameter[startpoint])
-            # Apparently only the startpoint of the branch is updated (???)
-            self.Aminus[startpoint]=mid_diameter[startpoint]**2/(4*self.neuron.diameter[startpoint]*self.neuron.length[startpoint]**2*self.neuron.Ri)
-            if endpoint>0:
-                self.Aplus[startpoint]=mid_diameter[startpoint]**2/(4*self.neuron.diameter[endpoint]*self.neuron.length[endpoint]**2*self.neuron.Ri)
-            else :
-                self.Aplus[startpoint]=gc
-                self.Aminus[startpoint]=gc
-            children_number+=1
-        
-        #boundary conditions
-        pointType = self.neuron.bc[endpoint]
-        hasChild = (children_number>0)
-        if (not hasChild) and (pointType == 1): #if the branch point is a leaf of the tree : apply default boundary condition
-            self.neuron.bc[endpoint] = 2 # Standard boundary condition
-        
-        #extract informations about the branches
-        index_ante = self.neuron.index[ante]
-        bp = endpoint
-        index = self.neuron.BPcount
-        self.i_list.append(i)
-        self.j_list.append(j)
-        self.bp_list.append(bp)
-        self.pointType_list.append(max(1,pointType))
-        self.pointTypeAnte_list.append(max(1,self.neuron.bc[ante]))
-        self.temp[index] = index_ante
-        self.id.append(index)
-        self.test_list.append((j-i+2)>1)
-        for x in xrange(j-i+2):
-            self.ante_list.append(ante)
-            self.post_list.append(bp)
-        if index_ante == 0:
-            self.ind0.append(index)
-        if pointType==0 :
-            self.ind_bctype_0.append(bp)
-        
-        
-        #initialize the parts of the linear systems that will not change
-        if (j-i+2)>1:    #j-i+2 = len(branch)
-            #initialize ab
-            self.ab[0,i:j]= self.Aplus[i:j]
-            self.ab[2,i:j]= self.Aminus[i+1:j+1]
-            
-            #initialize bL
-            VL0 = 1 * volt
-            self.bL[i] = (- VL0 * self.Aminus[i])
-            
-            #initialize bR
-            VR0 = 1 * volt
-            self.bR[j] = (- VR0 * self.Aplus[j+1])
-        
-        self.neuron.BPcount += 1
-        for x in (morphology.children):
-            self.prepare_branch(x,mid_diameter,endpoint)
-        
+        self.invr[morphology._origin]=0
+        for kid in (morphology.children):
+            self.cut_branches(kid)
+    
     def prepare(self):
         '''
-        From Hines 1984 paper, discrete formula is:
-        A_plus*V(i+1)-(A_plus+A_minus)*V(i)+A_minus*V(i-1)=Cm/dt*(V(i,t+dt)-V(i,t))+gtot(i)*V(i)-I0(i)
-       
-        A_plus: i->i+1
-        A_minus: i->i-1
-        
-        This gives the following tridiagonal system:
-        A_plus*V(i+1)-(Cm/dt+gtot(i)+A_plus+A_minus)*V(i)+A_minus*V(i-1)=-Cm/dt*V(i,t)-I0(i)
-        
+        Preparation of data structures.
+        See the relevant document.
         '''
-        mid_diameter = zeros(len(self.neuron)) # mid(i) : (i-1) <-> i
-        mid_diameter[1:] = .5*(self.neuron.diameter[:-1]+self.neuron.diameter[1:])
+        # Inverse axial resistance
+        self.invr=zeros(len(self.neuron))
+        self.invr[1:]=pi/(2*self.neuron.Ri)*(self.neuron.diameter[:-1]*self.neuron.diameter[1:])/\
+                   (self.neuron.length[:-1]+self.neuron.length[1:])
+        # Note: this would give nan for the soma
+        self.cut_branches(self.neuron.morphology)
         
-        self.Aplus = zeros(len(self.neuron)) # A+ i -> j = Aplus(j)
-        self.Aminus = zeros(len(self.neuron)) # A- i <- j = Aminus(j)
-        self.Aplus[1]= mid_diameter[1]**2/(4*self.neuron.diameter[1]*self.neuron.length[1]**2*self.neuron.Ri)
-        self.Aplus[2:]=mid_diameter[2:]**2/(4*self.neuron.diameter[1:-1]*self.neuron.length[1:-1]**2*self.neuron.Ri)
-        self.Aminus[1:]=mid_diameter[1:]**2/(4*self.neuron.diameter[1:]*self.neuron.length[1:]**2*self.neuron.Ri) 
+        # Linear systems
+        # The particular solution
+        '''a[i,j]=ab[u+i-j,j]''' # u is the number of upper diagonals = 1
+        self.ab_star=zeros((3,len(neuron)))
+        self.ab_star[0,1:]=self.invr[1:]/self.neuron.area[:-1]
+        self.ab_star[2,:-1]=self.invr[1:]/self.neuron.area[1:]
+        self.ab_star[1,:]=-neuron.Cm/neuron.clock.dt-self.invr/self.neuron.area
+        self.ab_star[1,:-1]-=self.invr[1:]/self.neuron.area[:-1]
+        # Homogeneous solutions
+        self.ab_plus=zeros((3,len(neuron)))
+        self.ab_minus=zeros((3,len(neuron)))
+        self.ab_plus[:]=self.ab_particular
+        self.ab_minus[:]=self.ab_particular
+        self.b_plus=zeros(len(neuron))
+        self.b_minus=zeros(len(neuron))
+        # Solutions
+        self.v_star=zeros(len(neuron))
+        self.u_plus=zeros(len(neuron))
+        self.u_minus=zeros(len(neuron))
         
-        self.neuron.index = zeros(len(self.neuron),int) # gives the index of the branch containing the current compartment
-        
-        self.neuron.BPcount = 0 # number of branch points (or branches). = len(self.neuron.branches)
-        
-        #the three solutions for V on a branch
-        self.vL = zeros((len(self.neuron)),numpy.float64)
-        self.vR = zeros((len(self.neuron)),numpy.float64)
-        self.d = zeros((len(self.neuron)),numpy.float64)
-        
-        #matrix and right hand in the tridiagonal systems that we solve to find vL, vR and d.
-        self.bL = zeros((len(self.neuron)),numpy.float64)
-        self.bR = zeros((len(self.neuron)),numpy.float64)
-        self.bd = zeros((len(self.neuron)),numpy.float64)
-        self.ab = zeros((3,len(self.neuron)))
-        self.ab1_base = zeros(len(self.neuron))
-        
-        
-        self.gtot = zeros(len(self.neuron))
-        self.I0 = zeros(len(self.neuron))
-        
-        self.i_list = [] #the indexes of the first points of the branches in the neuron. len = neuron.BPcount
-        self.j_list = [] #the indexes of the last points of the branches in the neuron. len = neuron.BPcount
-        self.bp_list = [] #the indexes of the branch points in the neuron. len = neuron.BPcount
-        self.pointType_list = [] #boundary condition on bp. len = neuron.BPcount
-        self.pointTypeAnte_list = [] #boundary condition on ante. len = neuron.BPcount
-        self.index_ante_list1 = [] #index of the parent branch of the current branch. index is in [0,neuron.BPcount]
-        self.index_ante_list2 = []
-        self.ante_list = [] #the indexes in the neuron of the branch points connected to i, for every compartment. len = len(self.neuron)
-        self.post_list = [] #for every compartment, contains the index of the branch point. len = len(self.neuron)
-        self.test_list = [] #for each branch : 1 if the branch has more than 3 compartments, else 0
-        
-        self.id = [] #list of every integer in [0,neuron.BPcount]. used in step to change some values in a matrix
-        
-        self.temp = zeros(len(self.neuron)) #used to construct index_ante_list0, 1, 2.
-        self.ind0 = [] #indexes (in [0,neuron.BPcount]) of the branches connected to compartment 0
-        self.ind_bctype_0 = [] #indexes of the branch point with boundary condition 0 (constant V)
-        
-        # prepare_branch : fill the lists, changes Aplus & Aminus
-        self.prepare_branch(self.neuron.morphology, mid_diameter,0)
-        
-        
-        self.index_ante_list1, self.ind1 = numpy.unique(numpy.array(self.temp,int),return_index=True)
-        self.ind1 = numpy.sort(self.ind1)
-        self.index_ante_list1 = self.temp[self.ind1]
-        self.index_ante_list1 = list(self.index_ante_list1)
-        self.ind2 = []
-        for x in xrange(self.neuron.BPcount):
-            self.ind2.append(x)
-        self.ind2 = numpy.delete(self.ind2,self.ind1,None) 
-        self.ind2 = numpy.setdiff1d(self.ind2, self.ind0, assume_unique=True)
-        self.index_ante_list2 = self.temp[self.ind2]
-        self.index_ante_list2 = list(self.index_ante_list2)
-        
-        self.index_ante_list = []
-        for idx in xrange(self.neuron.BPcount):
-            self.index_ante_list.append(self.temp[idx])
-        
-        # linear system P V = B used to deal with the voltage at branch points and take boundary conditions into account.
-        self.P = zeros((self.neuron.BPcount,self.neuron.BPcount))
-        self.B = zeros(self.neuron.BPcount)
-        self.solution_bp = zeros(self.neuron.BPcount)
-        
-        #in case of a sealed end, Aminus and Aplus are doubled :
-        self.Aminus_bp = self.Aminus[self.bp_list]
-        self.Aminus_bp [:] *= self.pointType_list[:]
-        self.Aplus_i = self.Aplus[self.i_list]
-        self.Aplus_i[:] *= self.pointTypeAnte_list[:]
-        
-    def step(self, neuron):           
-        self.gtot[:] = neuron._gtot #this compute the value of neuron._gtot.
-                            #if we call neuron._gtot[1] and then neuron._gtot[2] it does 2 computations
-                            #here we call it only one time on the whole array. this is much faster
-        self.I0 = neuron._I0
-        
-        #------------------------------------solve tridiagonal systems on the branchs-------------------------
-        #ab is the matrix in the tridiagonal systems describing the branches.
-        #bd is a right hand in one of these tridiagonal systems.
-        if self.neuron.changed : # neuron.changed = True <=> there was a new input somewhere. example : the user does  neuron.I[x] = y
-            self.update_ab_base() 
-        self.update_ab_gtot()
-        self.update_bd()
+        # Boundary conditions
+        self.boundary_conditions(self.neuron.morphology)
 
-        self.calculate_vd_vL_vR()
-        self.neuron.changed = False
-        
-        #-----------fill P and B, matrix and right hand used to find the voltage at the branch points-----------------
-        
-        self.P[:,:] = 0
-        self.B[:] = 0
-        
-        Cm = neuron.Cm[self.bp_list]
-        dt = neuron.clock.dt
-        gtot = self.gtot[self.bp_list]
-        I0 = self.I0[self.bp_list]
-        v_bp = neuron.v[self.bp_list]
-        vLleft = self.vL[self.i_list]
-        vLright = self.vL[self.j_list]
-        vRleft = self.vR[self.i_list]
-        vRright = self.vR[self.j_list]
-        dleft = self.d[self.i_list]
-        dright = self.d[self.j_list]
-        
-        vLleft[:] *= self.test_list[:] #if a branch has less than 3 compartments, this equals 0.
-                                        #thus we can do the same work on every branch point.
-        vLright[:] *= self.test_list[:]
-        vRleft[:] *= self.test_list[:]
-        vRright[:] *= self.test_list[:]
-        dleft[:] *= self.test_list[:]
-        dright[:] *= self.test_list[:]
-        
-        self.B[self.index_ante_list1] += - self.Aplus_i[self.ind1[:]] * dleft[self.ind1[:]]
-        self.B[self.index_ante_list2] += - self.Aplus_i[self.ind2[:]] * dleft[self.ind2[:]]
-        self.B[0] += sum(- self.Aplus_i[self.ind0[:]] * dleft[self.ind0[:]])
-        
-        self.P[(self.index_ante_list1,self.index_ante_list1)] += self.Aplus_i[self.ind1[:]] * (vLleft[self.ind1[:]] - 1)
-        self.P[(self.index_ante_list2,self.index_ante_list2)] += self.Aplus_i[self.ind2[:]] * (vLleft[self.ind2[:]] - 1)
-        self.P[0,0] += sum(self.Aplus_i[self.ind0[:]] * (vLleft[self.ind0[:]] - 1))
-        
-        di = diag_indices(neuron.BPcount)
-        
-        self.B[:] += - Cm[:]/dt * second * v_bp[:] - I0[:] - self.Aminus_bp[:] * dright[:]
-        self.P[di] += - Cm[:]/dt * second - gtot[:] + self.Aminus_bp[:] * (vRright[:] - 1)
-        self.P[(self.id,self.index_ante_list)] += self.Aminus_bp[:] *vLright[:]
-        self.P[(self.index_ante_list,self.id)] += self.Aplus_i[:] *vRleft[:]
-        
-        self.P[self.ind_bctype_0,:] = 0
-        self.P[(self.ind_bctype_0,self.ind_bctype_0)] = 1
-        self.B[self.ind_bctype_0] = neuron.v[self.ind_bctype_0]
-        
-        #------------------------------------------------------solve PV=B-----------------------------------
-        
-        self.solution_bp = solve(self.P,self.B)
-        neuron.v[self.bp_list] = self.solution_bp[:]
-        
-        #-------------------------------------------------------update v-------------------------------------
-        
-        self.finalize_v_global()
+    def boundary_conditions(self,morphology):
+        '''
+        Recursively sets the boundary conditions in the linear systems.
+        '''
+        first=morphology._origin # first compartment
+        last=first+len(morphology.x)-1 # last compartment
+        # Inverse axial resistances at the ends: r0 and rn
+        morphology.invr0=pi/(2*self.neuron.Ri)*self.neuron.diameter[first]**2/self.neuron.length[first]
+        morphology.invrn=pi/(2*self.neuron.Ri)*self.neuron.diameter[last]**2/self.neuron.length[last]
+        # Correction for boundary conditions
+        self.ab_star[1,first]-=morphology.invr0/self.neuron.area[first]
+        self.ab_star[1,last]-=morphology.invrn/self.neuron.area[last]
+        # RHS for homogeneous solutions
+        self.b_plus[last]=-morphology.invrn/self.neuron.area[last]
+        self.b_minus[first]=-morphology.invr0/self.neuron.area[first]
+        # Recursive call
+        for kid in (morphology.children):
+            self.boundary_conditions(kid)
 
-    def update_ab_base(self): #part of ab that doesn't change if there is no prompt from the operator.
-        self.ab1_base[:-1] = (- self.neuron.Cm[:-1] / self.neuron.clock.dt * second - self.Aminus[:-1] - self.Aplus[1:])
-        self.ab1_base[-1] = (- self.neuron.Cm[-1] / self.neuron.clock.dt * second - self.Aminus[-1])
-        
-    def update_ab_gtot(self): #this is called every step. changing part of ab.
-        self.ab[1,:] = self.ab1_base[:] - self.neuron._gtot
-        
-    def update_bd(self): #bd is a right hand side in a tridiagonal system
-        self.bd[:] = -self.neuron.Cm[:] / self.neuron.clock.dt * self.neuron.v[:] - self.neuron._I0[:]
-    
-    def calculate_vd_vL_vR(self):
-        for index in xrange(self.neuron.BPcount) :
-            if self.test_list[index] :
-                i = self.i_list[index]
-                j = self.j_list[index]
-                self.vL[i:j+1] = solve_banded((1,1),self.ab[:,i:j+1],self.bL[i:j+1],overwrite_ab=False,overwrite_b=False)
-                self.vR[i:j+1] = solve_banded((1,1),self.ab[:,i:j+1],self.bR[i:j+1],overwrite_ab=False,overwrite_b=False)
-                self.d[i:j+1] = solve_banded((1,1),self.ab[:,i:j+1],self.bd[i:j+1],overwrite_ab=False,overwrite_b=False)
-    
-    def finalize_v_global(self): #V(x) = V(left) * vL(x) + V(right) * vR(x) + d(x)
-        self.neuron.v[:] = self.vL[:] * self.neuron.v[self.ante_list[:]] + self.vR[:] * self.neuron.v[self.post_list[:]] + self.d[:]
-        self.neuron.v[self.bp_list] = self.solution_bp[:]
-    
     def __call__(self, neuron):
         '''
         Updates the state variables.
@@ -487,18 +293,50 @@ class SpatialStateUpdater_new(StateUpdater): # Modified by Remy
         if not self._isprepared:
             self.prepare()
             self._isprepared=True
-        #Update I,V
-        if neuron.changed :
-            self._state_updater.changed = True
-        self._state_updater(neuron) #update the currents
-        self.step(neuron) #update V
+        # Update the membrane currents (should it be after (no real difference though))
+        self._state_updater(neuron)
+        '''
+        x=solve_banded((lower,upper),ab,b)
+        lower = number of lower diagonals = 1
+        upper = number of upper diagonals = 1
+        ab = array(l+u+1,M)
+            each row is one diagonal
+        a[i,j]=ab[u+i-j,j]
         
+        That is:
+        a[i,j]=ab[1+i-j,j]
+        ab[1,:]=main diagonal
+        ab[0,1:]=upper diagonal
+        ab[2,:-1]=lower diagonal        '''
+        # Particular solution
+        b=-neuron.Cm/neuron.clock.dt*neuron.v-neuron._I0
+        ab = zeros((3,len(neuron)))
+        ab[:]=self.ab_star
+        ab[1,:]-=neuron._gtot
+        self.v_star[:]=solve_banded((1,1),ab,b,overwrite_ab=True,overwrite_b=True)
+        # Homogeneous solutions
+        b[:]=self.b_plus
+        ab[:]=self.ab_plus 
+        ab[1,:]-=neuron._gtot
+        self.u_plus[:]=solve_banded((1,1),ab,b,overwrite_ab=True,overwrite_b=True)
+        b[:]=self.b_minus
+        ab[:]=self.ab_minus 
+        ab[1,:]-=neuron._gtot
+        self.u_minus[:]=solve_banded((1,1),ab,b,overwrite_ab=True,overwrite_b=True)
+        '''
+        I'M HERE
+        TODO:
+        - fill the small linear system (sealed ends, etc)
+        - solve it
+        - deal with the soma
+        '''
+
     def __len__(self):
         '''
         Number of state variables
         '''
         return len(self.eqs)
-
+    
 SpatialStateUpdater = SpatialStateUpdater_new
 
 CompartmentalNeuron = SpatialNeuron
