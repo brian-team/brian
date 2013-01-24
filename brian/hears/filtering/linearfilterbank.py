d0bb16d87294625469c5fde62acaff6d1277380f
Marcel Stimberg <marcel.stimberg@ens.fr>
2013-01-24 15:00:33 +0000

    brian hears: Check for the useweave property at the creation of a LinearFilterbank (and not at module import), call the respective function in buffer_apply

--- brian/hears/filtering/linearfilterbank.py
+++ brian/hears/filtering/linearfilterbank.py
@@ -2,19 +2,11 @@ from brian import *
 from scipy import signal, weave, random
 from filterbank import Filterbank, RestructureFilterbank
 from ..bufferable import Bufferable
-try:
-    import numexpr
-except ImportError:
-    numexpr = None
 from itertools import izip
 
-# TODO: test all the buffered version of apply_linear_filterbank here
-# So far, they seem to more or less work, but probably need more thorough
-# testing.
+__all__ = ['LinearFilterbank']
 
-__all__ = ['LinearFilterbank','apply_linear_filterbank']
-
-def apply_linear_filterbank(b, a, x, zi):
+def _scipy_apply_linear_filterbank(b, a, x, zi):
     '''
     Parallel version of scipy lfilter command for a bank of n sequences of length 1
     
@@ -68,117 +60,54 @@ def apply_linear_filterbank(b, a, x, zi):
         o[:] = x
     return output
 
-if numexpr is not None and False:
-    def apply_linear_filterbank(b, a, x, zi):
-        X = x.copy()
-        output = empty_like(X)
-        for sample in xrange(X.shape[0]):
-            x = X[sample]
-            for curf in xrange(zi.shape[2]):
-                #y = b[:, 0, curf]*x+zi[:, 0, curf]
-                y = numexpr.evaluate('b*x+zi', local_dict={
-                        'b':b[:, 0, curf],
-                        'x':x,
-                        'zi':zi[:, 0, curf]})
-                for i in xrange(b.shape[1]-2):
-                    #zi[:, i, curf] = b[:, i+1, curf]*x+zi[:, i+1, curf]-a[:, i+1, curf]*y
-                    zi[:, i, curf] = numexpr.evaluate('b*x+zi-a*y', local_dict={
-                                            'b':b[:, i+1, curf],
-                                            'x':x,
-                                            'zi':zi[:, i+1, curf],
-                                            'a':a[:, i+1, curf],
-                                            'y':y})
-                i = b.shape[1]-2
-                #zi[:, i, curf] = b[:, i+1, curf]*x-a[:, i+1, curf]*y
-                zi[:, i, curf] = numexpr.evaluate('b*x-a*y', local_dict={
-                                        'b':b[:, i+1, curf],
-                                        'x':x,
-                                        'a':a[:, i+1, curf],
-                                        'y':y})
-                x = y
-            output[sample] = y
-        return output
-    
 
-# TODO: accelerate this even more using SWIG instead of weave?
-if get_global_preference('useweave'):
-    _cpp_compiler = get_global_preference('weavecompiler')
-    _extra_compile_args = ['-O3']
-    if _cpp_compiler=='gcc':
-        _extra_compile_args += get_global_preference('gcc_options')
-    _old_apply_linear_filterbank = apply_linear_filterbank
-    
-    def apply_linear_filterbank(b, a, x, zi):
-        if zi.shape[2]>1:
-            # we need to do this so as not to alter the values in x in the C code below
-            # but if zi.shape[2] is 1 there is only one filter in the chain and the
-            # copy operation at the end of the C code will never happen.
-            x = array(x, copy=True, order='C')
-        else:
-            # make sure that the array is in C-order
-            x = asarray(x, order='C')
-        y = empty_like(x)
-        n, m, p = b.shape
-        n1, m1, p1 = a.shape
-        numsamples = x.shape[0]
-        if n1!=n or m1!=m or p1!=p or x.shape!=(numsamples, n) or zi.shape!=(n, m, p):
-            raise ValueError('Data has wrong shape.')
-        if numsamples>1 and not x.flags['C_CONTIGUOUS']:
-            raise ValueError('Input data must be C_CONTIGUOUS')
-        if not b.flags['F_CONTIGUOUS'] or not a.flags['F_CONTIGUOUS'] or not zi.flags['F_CONTIGUOUS']:
-            raise ValueError('Filter parameters must be F_CONTIGUOUS')
-        code = '''
-        #define X(s,i) x[(s)*n+(i)]
-        #define Y(s,i) y[(s)*n+(i)]
-        #define A(i,j,k) a[(i)+(j)*n+(k)*n*m]
-        #define B(i,j,k) b[(i)+(j)*n+(k)*n*m]
-        #define Zi(i,j,k) zi[(i)+(j)*n+(k)*n*(m-1)]
-        for(int s=0; s<numsamples; s++)
+def _weave_apply_linear_filterbank(b, a, x, zi,
+                                   cpp_compiler, extra_compile_args):
+    if zi.shape[2]>1:
+        # we need to do this so as not to alter the values in x in the C code below
+        # but if zi.shape[2] is 1 there is only one filter in the chain and the
+        # copy operation at the end of the C code will never happen.
+        x = array(x, copy=True, order='C')
+    else:
+        # make sure that the array is in C-order
+        x = asarray(x, order='C')
+    y = empty_like(x)
+    n, m, p = b.shape
+    n1, m1, p1 = a.shape
+    numsamples = x.shape[0]
+    if n1!=n or m1!=m or p1!=p or x.shape!=(numsamples, n) or zi.shape!=(n, m, p):
+        raise ValueError('Data has wrong shape.')
+    if numsamples>1 and not x.flags['C_CONTIGUOUS']:
+        raise ValueError('Input data must be C_CONTIGUOUS')
+    if not b.flags['F_CONTIGUOUS'] or not a.flags['F_CONTIGUOUS'] or not zi.flags['F_CONTIGUOUS']:
+        raise ValueError('Filter parameters must be F_CONTIGUOUS')
+    code = '''
+    #define X(s,i) x[(s)*n+(i)]
+    #define Y(s,i) y[(s)*n+(i)]
+    #define A(i,j,k) a[(i)+(j)*n+(k)*n*m]
+    #define B(i,j,k) b[(i)+(j)*n+(k)*n*m]
+    #define Zi(i,j,k) zi[(i)+(j)*n+(k)*n*(m-1)]
+    for(int s=0; s<numsamples; s++)
+    {
+        for(int k=0; k<p; k++)
         {
-            for(int k=0; k<p; k++)
-            {
-                for(int j=0; j<n; j++)
-                             Y(s,j) =   B(j,0,k)*X(s,j) + Zi(j,0,k);
-                for(int i=0; i<m-2; i++)
-                    for(int j=0;j<n;j++)
-                        Zi(j,i,k) = B(j,i+1,k)*X(s,j) + Zi(j,i+1,k) - A(j,i+1,k)*Y(s,j);
-                for(int j=0; j<n; j++)
-                      Zi(j,m-2,k) = B(j,m-1,k)*X(s,j)               - A(j,m-1,k)*Y(s,j);
-                if(k<p-1)
-                    for(int j=0; j<n; j++)
-                        X(s,j) = Y(s,j);
-            }
-        }
-        '''
-        if 0: # this version is used for comparing without vectorising over frequency
-            code = '''
-            #define X(s,i) x[(s)*n+(i)]
-            #define Y(s,i) y[(s)*n+(i)]
-            #define A(i,j,k) a[(i)+(j)*n+(k)*n*m]
-            #define B(i,j,k) b[(i)+(j)*n+(k)*n*m]
-            #define Zi(i,j,k) zi[(i)+(j)*n+(k)*n*(m-1)]
             for(int j=0; j<n; j++)
-            {
-                for(int k=0; k<p; k++)
-                {
-                    for(int s=0; s<numsamples; s++)
-                    {
-                    
-                        Y(s,j) =   B(j,0,k)*X(s,j) + Zi(j,0,k);
-                        for(int i=0; i<m-2; i++)
-                            Zi(j,i,k) = B(j,i+1,k)*X(s,j) + Zi(j,i+1,k) - A(j,i+1,k)*Y(s,j);                    
-                        Zi(j,m-2,k) = B(j,m-1,k)*X(s,j)               - A(j,m-1,k)*Y(s,j);
-                        if(k<p-1)
-                            X(s,j) = Y(s,j);
-                    }
-                }
-            }
-            '''
-        weave.inline(code, ['b', 'a', 'x', 'zi', 'y', 'n', 'm', 'p', 'numsamples'],
-                     compiler=_cpp_compiler,
-                     extra_compile_args=_extra_compile_args)
-        return y
-    apply_linear_filterbank.__doc__ = _old_apply_linear_filterbank.__doc__
+                         Y(s,j) =   B(j,0,k)*X(s,j) + Zi(j,0,k);
+            for(int i=0; i<m-2; i++)
+                for(int j=0;j<n;j++)
+                    Zi(j,i,k) = B(j,i+1,k)*X(s,j) + Zi(j,i+1,k) - A(j,i+1,k)*Y(s,j);
+            for(int j=0; j<n; j++)
+                  Zi(j,m-2,k) = B(j,m-1,k)*X(s,j)               - A(j,m-1,k)*Y(s,j);
+            if(k<p-1)
+                for(int j=0; j<n; j++)
+                    X(s,j) = Y(s,j);
+        }
+    }
+    '''
+    weave.inline(code, ['b', 'a', 'x', 'zi', 'y', 'n', 'm', 'p', 'numsamples'],
+                 compiler=cpp_compiler,
+                 extra_compile_args=extra_compile_args)
+    return y 
 
 
 class LinearFilterbank(Filterbank):
@@ -249,6 +178,14 @@ class LinearFilterbank(Filterbank):
         self.filt_b = array(b, order='F')
         self.filt_a = array(a, order='F')
         self.filt_state = zeros((b.shape[0], b.shape[1], b.shape[2]), order='F')
+        self.use_weave = get_global_preference('useweave')
+        if self.use_weave:
+            log_info('brian.hears.filtering.linearfilterbank', 'Using weave')
+            self.cpp_compiler = get_global_preference('weavecompiler')
+            self.extra_compile_args = ['-O3']
+            if self.cpp_compiler=='gcc':
+                self.extra_compile_args += get_global_preference('gcc_options')
+
 
     def reset(self):
         self.buffer_init()
@@ -259,8 +196,13 @@ class LinearFilterbank(Filterbank):
     
     def buffer_apply(self, input):
         
-        return apply_linear_filterbank(self.filt_b, self.filt_a, input,
-                                       self.filt_state)
+        if self.use_weave:
+            return _weave_apply_linear_filterbank(self.filt_b, self.filt_a, input,
+                                                  self.filt_state, self.cpp_compiler,
+                                                  self.extra_compile_args)
+        else:
+            return _scipy_apply_linear_filterbank(self.filt_b, self.filt_a, input,
+                                                  self.filt_state)
         
     def decascade(self, ncascade=1):
         '''
